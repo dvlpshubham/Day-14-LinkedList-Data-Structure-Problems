5591580ac03e01da000e413cea2146a619045438
Shubham Raj <shubhamaryan805@gmail.com>
2022-08-03 13:13:04 +0530

    Added : Linked List Class

---
+++ datastructure/LinkedList.java
@@ -0,0 +1,18 @@
+package datastructure;
+
+public class LinkedList  {
+    Node head =null;
+    class Node{
+        Object data;
+        Node Next = null;    //next is a reference
+
+        public Node(Object data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Linked list !!!!");
+
+    }
+}
